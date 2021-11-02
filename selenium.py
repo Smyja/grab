d5190f01fae8144e63efe3310e57e942f9df4b66
Smyja <akposlive59@gmail.com>
2021-11-02 11:28:41 +0100

    Oops

--- selenium.py
+++ selenium.py
@@ -7,6 +7,7 @@
 
 import time
 import json
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
